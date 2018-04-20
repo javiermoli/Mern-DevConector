a9c3896a205e94c0dbe4230134cc4a98d072e6d8
Javier Molinas <javomolinas91@gmail.com>
2018-04-20 17:30:31 -0300

    Initial express server with routes files

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+   'mongodb://javomolinas:javo3506@ds225608.mlab.com:25608/devconector'
+}
---
+++ routes/api/users.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+
+// @route GET api/users/test
+// @desc tests users route
+// @acces public
+router.get('/test', (req, res) => res.json({msg:"users works"}))
+
+module.exports = router;
