04a218dffdd200d663b5bfc7d140ed0d116ca0eb
Javier Molinas <javomolinas91@gmail.com>
2018-04-28 09:18:40 -0300

    Add register validation, login validation, profile and register profile

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,10 @@ const jwt = require('jsonwebtoken')
 const keys = require('../../config/keys')
 const passport = require('passport')
 
+//Load Input Validation
+const validateRegisterInput = require('../../validation/register')
+const validateLoginInput = require('../../validation/login')
+
 // Load user model
 const User = require('../../models/User')
 // @route GET api/users/test
@@ -17,11 +21,18 @@ router.get('/test', (req, res) => res.json({msg:"users works"}))
 // @route GET api/users/register
 // @desc Rgister route
 // @acces public
-
 router.post('/register', (req, res) => {
-  User.findOne({email: req.body.email}).then(user => {
+  const { errors, isValid } = validateRegisterInput(req.body)
+
+//Check validation
+  if(!isValid) {
+    return res.status(400).json(errors)
+  }
+  const email = req.body.email
+  User.findOne({email}).then(user => {
     if(user) {
-      return res.status(400).json({email: 'Email already exists'})
+      errors.email = 'Email already exists'
+      return res.status(400).json(errors)
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: '200', //Size
@@ -54,6 +65,14 @@ router.post('/register', (req, res) => {
 // @desc Login USer / Returning JWT token
 // @acces public
 router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body)
+
+
+//Check validation
+  if(!isValid) {
+    return res.status(400).json(errors)
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
@@ -62,16 +81,15 @@ router.post('/login', (req, res) => {
     .then(user => {
       //check for users
       if(!user) {
-        return res.status(404).json({email: 'User not found'});
+        errors.email = 'User not found'
+        return res.status(404).json(errors.email);
       }
       //check passwrod
       bcrypt.compare(password, user.password)
       .then(isMatch => {
         if(isMatch) {
-          //USer Matched
-
-           // Create JWT payload
-          const payload = { id: user.id, name: user.name, avatar: user.avatar }
+          //User Matched
+      const payload = { id: user.id, name: user.name, avatar: user.avatar } // Create JWT payload
 
           //Sign Token
           jwt.sign(payload,
@@ -84,8 +102,9 @@ router.post('/login', (req, res) => {
               })
           })
 
-        }else {
-          return res.status(400).json({password:'Password incorrect'})
+        } else {
+          errors.password = 'Password incorrect'
+          return res.status(400).json(errors.password)
         }
       })
     })
