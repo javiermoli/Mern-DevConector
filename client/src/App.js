de01b942dbdce4dc20b29aa01fbaa8cf26567fc8
Javier Molinas <javomolinas91@gmail.com>
2018-05-02 21:39:31 -0300

    Conect front with back and register validation

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 import NavBar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import Landing from './components/layout/Landing'
+import Login from "./components/auth/Login"
+import Register from "./components/auth/Register"
+
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <NavBar/>
-        <Landing/>
-        <Footer/>
-      </div>
-    );
+    return <Router>
+        <div className="App">
+          <NavBar />
+          <Route exact path="/" component={Landing} />
+          <div>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+          <Footer />
+        </div>
+      </Router>;
   }
 }
 
